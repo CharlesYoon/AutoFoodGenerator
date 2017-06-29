9c6f62de9962b0406fcecf46f4c8fb5bc4950315
Charles <ccyoon2@illinois.edu>
2017-06-29 18:40:32 +0200

    first commit

---
+++ AutoFoodGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoFoodGenerator
+//
+//  Created by Charles Yoon on 6/29/17.
+//  Copyright © 2017 Charles Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
