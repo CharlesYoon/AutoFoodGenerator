5657b0ebbdb9b6c419cbcd782b0c0865deafc1ae
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-06-30 14:29:59 +0200

    Recipe UI

---
+++ AutoFoodGenerator/recipesTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  recipesTableViewController.swift
+//  AutoFoodGenerator
+//
+//  Created by Nathan Lewis on 6/29/17.
+//  Copyright © 2017 Charles Yoon. All rights reserved.
+//
+
+import UIKit
+
+class recipesTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    let asianFood = ["Chicken Chow Mein","Orange Chicken","Dim Sum"]
+    let starRatings = ["★★★★☆","★★☆☆☆","★★★★★"]
+    
+    lazy var searchBar:UISearchBar = UISearchBar()
+    
+    
+    func searchBar(searchBar: UISearchBar, textDidChange textSearched: String)
+    {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBar.searchBarStyle = UISearchBarStyle.prominent
+        searchBar.placeholder = " Search..."
+        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self as UISearchBarDelegate
+        navigationItem.titleView = searchBar
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return (asianFood.count)
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! recipesTableViewCell
+
+        // Configure the cell...
+        cell.recipesImage.image = UIImage(named: asianFood[indexPath.row] + ".jpg")
+        cell.recipesLabel.text = asianFood[indexPath.row]
+        cell.starsLabel.text = starRatings[indexPath.row]
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
